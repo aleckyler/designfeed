00a576b7e7674c31002164d6c703c39d54c9eaa6
Alec Greenberg <aleckyler@comcast.com>
2015-08-13 10:20:01 -0400

    added migrations to add user_id and submission_id to profile and review

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :submissions, :responses
+  has_many :submissions
+  has_many :responses
   has_one :profile
 end

e7b0fa93893e3657505a33808a676cf9edf0a653
Alec Greenberg <aleckyler@comcast.com>
2015-08-21 12:50:45 -0400

    refactored vote buttons

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,34 +1,42 @@
 class VotesController < ApplicationController
 
   def upvote
-    @vote = Vote.new
-    @submission = Submission.find(params[:submission_id])
-    @vote.submission = @submission
-    @vote.user = current_user
-    @vote.up_or_down = true
-    if @vote.save
-      redirect_to(@submission)
+    if !user_signed_in?
+      redirect_to new_user_session_path, notice: 'You must sign in to vote'
     else
-      @old_vote = Vote.find_by(user_id: current_user.id, submission_id: params[:submission_id])
-      @old_vote.destroy
-      @vote.save
-      redirect_to(@submission)
+      @vote = Vote.new
+      @submission = Submission.find(params[:submission_id])
+      @vote.submission = @submission
+      @vote.user = current_user
+      @vote.up_or_down = true
+      if @vote.save
+        redirect_to(@submission)
+      else
+        @old_vote = Vote.find_by(user_id: current_user.id, submission_id: params[:submission_id])
+        @old_vote.destroy
+        @vote.save
+        redirect_to(@submission)
+      end
     end
   end
 
   def downvote
-    @vote = Vote.new
-    @submission = Submission.find(params[:submission_id])
-    @vote.submission = @submission
-    @vote.user = current_user
-    @vote.up_or_down = false
-    if @vote.save
-      redirect_to(@submission)
+    if !user_signed_in?
+      redirect_to new_user_session_path, notice: 'You must sign in to vote'
     else
-      @old_vote = Vote.find_by(user_id: current_user.id, submission_id: params[:submission_id])
-      @old_vote.destroy
-      @vote.save
-      redirect_to(@submission)
+      @vote = Vote.new
+      @submission = Submission.find(params[:submission_id])
+      @vote.submission = @submission
+      @vote.user = current_user
+      @vote.up_or_down = false
+      if @vote.save
+        redirect_to(@submission)
+      else
+        @old_vote = Vote.find_by(user_id: current_user.id, submission_id: params[:submission_id])
+        @old_vote.destroy
+        @vote.save
+        redirect_to(@submission)
+      end
     end
   end
 end
